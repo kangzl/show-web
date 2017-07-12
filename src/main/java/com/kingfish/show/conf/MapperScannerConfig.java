0c383b920c79c26fa41296bd8e1c3949a69fb0aa
zhenle.kzl <zhenle.kzl@alibaba-inc.com>
2017-07-12 13:43:30 +0800

    add mybatis generator

---
+++ src/main/java/com/kingfish/show/conf/MapperScannerConfig.java
@@ -0,0 +1,21 @@
+package com.kingfish.show.conf;
+
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by kingfish on 2017/7/11.
+ */
+@Configuration
+@AutoConfigureAfter(MybatisAutoConfiguration.class)
+public class MapperScannerConfig {
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setBasePackage("com.kingfish.show.mybatis.dao");
+        return mapperScannerConfigurer;
+    }
+}
