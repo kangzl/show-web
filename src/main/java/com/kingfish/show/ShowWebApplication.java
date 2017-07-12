8e224fa8099784ebb9b4028097a6328dd4f78d50
zhenle.kzl <zhenle.kzl@alibaba-inc.com>
2017-07-12 10:32:20 +0800

    init

---
+++ src/main/java/com/kingfish/show/ShowWebApplication.java
@@ -0,0 +1,12 @@
+package com.kingfish.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowWebApplication.class, args);
+	}
+}
