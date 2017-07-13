921031f9b2417951470be2299adeebe6911ed821
zhenle.kzl <zhenle.kzl@alibaba-inc.com>
2017-07-13 23:50:25 +0800

    add wookmark example

--- src/main/java/com/kingfish/show/controller/WookmarkExampleController.java
+++ src/main/java/com/kingfish/show/controller/WookmarkExampleController.java
@@ -10,8 +10,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WookmarkExampleController {
 
+    @RequestMapping("example.htm")
+    public String example(Model mode) {
+        return "wookmark-example/example";
+    }
+
+    @RequestMapping("example-amd.htm")
+    public String exampleAmd(Model mode) {
+        return "wookmark-example/example-amd/index";
+    }
+
+    @RequestMapping("example-and-filter.htm")
+    public String exampleAndFilter(Model mode) {
+        return "wookmark-example/example-filter/and-filter";
+    }
+
+    @RequestMapping("example-fade.htm")
+    public String exampleFade(Model mode) {
+        return "wookmark-example/example-filter/fade";
+    }
+
+    @RequestMapping("example-index.htm")
+    public String exampleIndex(Model mode) {
+        return "wookmark-example/example-filter/index";
+    }
+
+    @RequestMapping("example-only-filter.htm")
+    public String exampleOnlyFilter(Model mode) {
+        return "wookmark-example/example-filter/only-filter";
+    }
+
     @RequestMapping("example-endless-scroll.htm")
-    public String test(Model mode) {
+    public String exampleEndlessScroll(Model mode) {
         return "wookmark-example/example-endless-scroll";
     }
+
+    @RequestMapping("example-flexible.htm")
+    public String exampleFlexible(Model mode) {
+        return "wookmark-example/example-flexible";
+    }
+
+    @RequestMapping("example-lightbox.htm")
+    public String exampleLightbox(Model mode) {
+        return "wookmark-example/example-lightbox";
+    }
+
+    @RequestMapping("example-load-images.htm")
+    public String exampleLoadImages(Model mode) {
+        return "wookmark-example/example-load-images";
+    }
+
+    @RequestMapping("example-placeholder.htm")
+    public String examplePlaceholder(Model mode) {
+        return "wookmark-example/example-placeholder";
+    }
+
+    @RequestMapping("example-sort.htm")
+    public String exampleSort(Model mode) {
+        return "wookmark-example/example-sort";
+    }
+
+    @RequestMapping("example-stamp.htm")
+    public String exampleStamp(Model mode) {
+        return "wookmark-example/example-stamp";
+    }
+
 }
