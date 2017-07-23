386e96b674403c87b929e99d8f603f724e1036d5
zhenle.kzl <zhenle.kzl@alibaba-inc.com>
2017-07-24 02:07:49 +0800

    add cache

--- src/main/java/com/kingfish/show/ShowWebApplication.java
+++ src/main/java/com/kingfish/show/ShowWebApplication.java
@@ -2,8 +2,10 @@ package com.kingfish.show;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ShowWebApplication {
 
 	public static void main(String[] args) {
